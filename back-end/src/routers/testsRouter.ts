a997f0a548e09c26adc799c3dd82602cb2dc23a9
Liaess <yann.melo@gmail.com>
2022-06-17 12:52:37 -0300

    Preparando revisão de código

--- back-end/src/routers/testsRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { recommendationController } from "../controllers/recommendationController.js";
-const testsRouter = Router();
-
-testsRouter.post("/reset", recommendationController.removeAll);
-testsRouter.post("", recommendationController.insert);
-
-export default testsRouter;
